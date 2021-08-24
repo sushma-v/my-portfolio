5a1b6b2e20ddd238d2e58cdf889d4299d7715100
Sushma Venugopal <sushma@Sushmas-MBP-2.fritz.box>
2021-08-24 19:10:57 +0100

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -9,4 +9,11 @@ class Portfolio < ApplicationRecord
   	def self.angular
   		where(subtitle: 'Angular')
   	end
+
+  	after_initialize :set_defaults
+
+  	def set_defaults
+  		self.main_image ||= "https://place-hold.it/600x400"
+  		self.thumb_image ||= "https://place-hold.it/350x200"
+  	end
 end
